657fd1eb60aa3a1ed3e32cea9c1fbd81b57e6e2a
jaqarrick <jcarrick@protonmail.com>
2021-12-27 19:57:05 -0700

    Update readme text piped to info file

--- jours.sh
+++ jours.sh
@@ -169,79 +169,37 @@ function init_journal() {
 	echo "AUTHOR='$AUTHOR'" >>~/.joursconfig
 	echo "
 Jours is a simple CLI designed for safe and secure journaling.
-Getting started
 
-After installing the cli run
-
-jours init
-
-You will be prompted to input a location for your journal. This defaults to ~/Documents/jours
-
-You will then be prompted to create a password for your journal. Just hit enter to let jours recommend a password.
-
-IMPORTANT: If you forget or lose your password, you will not have access to any of your encrypted entries!
-
-Once initiated, you can run
-
-jours login
-
-You'll be prompted for a password.
 Creating an entry
-
-Compose an entry with:
+Compose today's entry with:
 
 jours compose
-
-Or, edit a specific entry with
-
-jours compose <date>
-
-<date> is in YYYY-MM-DD format, for example:
-
-jours compose 2011-11-31
-
 Reading your entries
+If you want to read all entries, a single entry, or a collection of entries without editing run
 
-If you want to read a single entry or collection of entries without editing run
+jours read all 
+
+# or
 
 jours read <date>
 
 # or
 
 jours read <month>
-
 <month> is in YYYY-MM format and concatinates all entries from a given month into a single text file.
 
-Read today's entry:
+The read command decrypts all relevant entries and prints the result in the /exposed/<journal name>.txt.
 
-jours read today
-
-Read this month's entries:
-
-jours read month
-
-Read this year's entries:
-
-jours read year
-
-You can read your entire journal with
-
-jours read all
-
-The read command decrypts all relevant entries and prints the result in the /exposed/journal.txt.
 Logging out
-
 To logout from jours simply type
 
 jours logout
+This encrypts all unencrypted entries, removes any exposed journals read during your login session, and locks the /entries directory.
 
-This encrypts all unencrypted entries, removes the journal.txt file, and locks the /entries directory.
 Switching Journals
-
 Jours supports multiple journals to keep your thoughts and notes more organized. To create a new one use
 
 jours create <journal name>
-
 To switch journals while logged in use
 
 jours switch 
@@ -249,9 +207,7 @@ jours switch
 # or
 
 jours switch <journal name>
-
 Locking entries
-
 If you want to manually lock or unlock entries while logged in, use the lock or unlock command.
 
 To lock/unlock the entry for today's date just run
@@ -261,7 +217,6 @@ jours lock
 # or 
 
 jours unlock
-
 To lock/unlock a specific entry run
 
 jours lock <date>
@@ -269,7 +224,6 @@ jours lock <date>
 # or
 
 jours unlock <date>
-
 To lock/unlock all entries run
 
 jours lock all
@@ -277,12 +231,10 @@ jours lock all
 # or
 
 jours unlock all
-
 Changing your password
 To change your password use
 
 jours rehash
-
 You'll be asked your current password and prompted to enter a new one.
 " >readme.txt
 
